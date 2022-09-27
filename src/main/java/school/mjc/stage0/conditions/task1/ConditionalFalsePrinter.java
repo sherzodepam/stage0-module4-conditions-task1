8535d8c33276346e4e345c382910d92a01f94cef
Sherzod <sherzodd7707@gmail.com>
2022-09-27 10:21:03 +0500

    Fifth Attempt

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,11 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
-        if (true){
-            System.out.println(false);
+        if (false){
+            System.out.println(true);
         }
         else{
-            System.out.println(true);
+            System.out.println(false);
         }
     }
 }
